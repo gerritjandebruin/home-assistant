6b2fdfedb3a9ac37c927e4362e3f2953c36d852f
Gerrit Jan de Bruin <gerritjan.debruin@gmail.com>
2025-06-27 11:27:55 +0200

    gitignore custom_components

--- custom_components/shell_recharge/const.py
+++
@@ -1,7 +0,0 @@
-"""Constants for the shell_recharge integration."""
-from datetime import timedelta
-
-DOMAIN = "shell_recharge"
-SerialNumber = str
-EvseId = str
-UPDATE_INTERVAL = timedelta(minutes=5)
